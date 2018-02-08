93c029f58ed94df19baf03493ba2f81533e008dc
Preznyák László <laszlopreznyak11@gmail.com>
2018-02-08 21:17:28 +0200

    Main...

--- src/main/java/hu/preznyak/Main.java
+++ src/main/java/hu/preznyak/Main.java
@@ -1,13 +1,14 @@
 package hu.preznyak;
 
+import hu.preznyak.daos.ServiceDAO;
 import hu.preznyak.daos.UserDAO;
+import hu.preznyak.daos.impls.ServiceDAOImpl;
 import hu.preznyak.daos.impls.UserDAOImpl;
+import hu.preznyak.entities.Service;
 import hu.preznyak.entities.User;
+import hu.preznyak.enums.ServiceType;
 
 public class Main {
     public static void main(String[] args){
-        UserDAO userDAO = new UserDAOImpl();
-        User newUser = userDAO.getUser("preznyak","preznyakpw");
-        System.out.println(newUser.getUsername());
     }
 }
