46a820c26777ce7d536ff43235d1e345e5dc789b
Preznyák László <laszlopreznyak11@gmail.com>
2018-02-22 14:01:03 +0200

    Javadoc added to Service entity class.

--- src/main/java/hu/preznyak/entities/Service.java
+++ src/main/java/hu/preznyak/entities/Service.java
@@ -4,33 +4,72 @@ import hu.preznyak.enums.ServiceType;
 
 import javax.persistence.*;
 
+/**
+ * <h1>Service Entity Class</h1>
+ * This is an entity class for services.
+ *
+ * @author Preznyák László
+ * @version 1.0
+ */
+
 @Table
 @Entity(name = "service")
 public class Service {
 
+    /**
+     * The id of a service.
+     * It is a generated value.
+     */
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private int id;
 
+    /**
+     * Name of the service.
+     * It's not nullable.
+     */
     @Column(nullable = false)
     private String serviceName;
 
+    /**
+     * Type of the service.
+     */
     @Column
     @Enumerated(EnumType.STRING)
     private ServiceType serviceType;
 
+    /**
+     * Description about the service.
+     */
     @Column
     private String description;
 
+    /**
+     * Address for the service.
+     */
     @OneToOne(cascade = CascadeType.ALL)
     private Address address;
 
+    /**
+     * Website link for the service.
+     */
     @Column
     private String website;
 
+    /**
+     * No-arg constructor for service entity.
+     */
     public Service() {
     }
 
+    /**
+     * Constructor for service entity with all variables.
+     * @param serviceType Type of the service.
+     * @param description Description about the service.
+     * @param address Address fot the service.
+     * @param website Wwebsite of the service.
+     * @param serviceName Name of the service.
+     */
     public Service(ServiceType serviceType, String description, Address address, String website, String serviceName) {
         this.serviceType = serviceType;
         this.description = description;
@@ -39,54 +78,106 @@ public class Service {
         this.serviceName = serviceName;
     }
 
+    /**
+     * Getter for id.
+     * @return id of service.
+     */
     public int getId() {
         return id;
     }
 
+    /**
+     * Setter for id.
+     * @param id the id of the service.
+     */
     public void setId(int id) {
         this.id = id;
     }
 
+    /**
+     * Getter for type of the service.
+     * @return serviceType the type of the service.
+     */
     public ServiceType getServiceType() {
         return serviceType;
     }
 
+    /**
+     * Setter for type of the service.
+     * @param serviceType the type of the service.
+     */
     public void setServiceType(ServiceType serviceType) {
         this.serviceType = serviceType;
     }
 
+    /**
+     * Getter for description of the service.
+     * @return description the description of the service.
+     */
     public String getDescription() {
         return description;
     }
 
+    /**
+     * Setter for description.
+     * @param description the description of the sercice.
+     */
     public void setDescription(String description) {
         this.description = description;
     }
 
+    /**
+     * Getter for address.
+     * @return address the address for the service.
+     */
     public Address getAddress() {
         return address;
     }
 
+    /**
+     * Setter for address.
+     * @param address the address for the service.
+     */
     public void setAddress(Address address) {
         this.address = address;
     }
 
+    /**
+     * Getter for website.
+     * @return website the website of the service.
+     */
     public String getWebsite() {
         return website;
     }
 
+    /**
+     * Setter for website.
+     * @param website the website of the service.
+     */
     public void setWebsite(String website) {
         this.website = website;
     }
 
+    /**
+     * Getter for the name of the service.
+     * @return serviceName the name of the service.
+     */
     public String getServiceName() {
         return serviceName;
     }
 
+    /**
+     * Setter for the name of the service.
+     * @param serviceName the name of the service.
+     */
     public void setServiceName(String serviceName) {
         this.serviceName = serviceName;
     }
 
+    /**
+     * ToString method of the Service class.
+     * @return String format of a Service object.
+     */
     @Override
     public String toString() {
         return "Service{" +
