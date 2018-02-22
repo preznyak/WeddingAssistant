52d656724ed9230d73ce85dae90f1aa33e95d23d
Preznyák László <laszlopreznyak11@gmail.com>
2018-02-22 16:35:41 +0200

    Javadoc addded to utilities

--- src/main/java/hu/preznyak/utils/SessionUtils.java
+++ src/main/java/hu/preznyak/utils/SessionUtils.java
@@ -3,8 +3,19 @@ package hu.preznyak.utils;
 import javax.faces.context.FacesContext;
 import javax.servlet.http.HttpSession;
 
+/**
+ * <h1>SessionUtils class.</h1>
+ *
+ * @author Preznyák László
+ * @version 1.0
+ */
+
 public class SessionUtils {
 
+    /**
+     * getSession method for providing a session.
+     * @return A HttpSession object.
+     */
     public static HttpSession getSession(){
         return (HttpSession) FacesContext.getCurrentInstance()
                 .getExternalContext().getSession(false);
--- src/main/java/hu/preznyak/utils/SingletonEMFactory.java
+++ src/main/java/hu/preznyak/utils/SingletonEMFactory.java
@@ -3,13 +3,32 @@ package hu.preznyak.utils;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
+/**
+ * <h1>SingletonEntityManagerFactory class</h1>
+ * This is a class for creating a single entity manager object.
+ *
+ * @author Preznyák László
+ * @version 1.0
+ */
+
 public class SingletonEMFactory {
 
+    /**
+     * This is a static EntityManagerFactory object.
+     */
     private static EntityManagerFactory emf = Persistence.createEntityManagerFactory("waunit");
 
+    /**
+     * This is a no-arg constructor of the class.
+     */
     public SingletonEMFactory() {
     }
 
+    /**
+     * This static method is a getter method.
+     * By this method the entity manager factory could be used.
+     * @return emf The entity manager factory.
+     */
     public static EntityManagerFactory getFactory(){
         return emf;
     }
