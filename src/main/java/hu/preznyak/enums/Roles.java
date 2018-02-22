e9ba104e596b807697bc4ce08e05cfcd17cf9d85
Preznyák László <laszlopreznyak11@gmail.com>
2018-02-22 16:29:30 +0200

    Javadoc added to enums

--- src/main/java/hu/preznyak/enums/Roles.java
+++ src/main/java/hu/preznyak/enums/Roles.java
@@ -1,5 +1,13 @@
 package hu.preznyak.enums;
 
+/**
+ * <h1>Roles enumeration for user roles.</h1>
+ * This is an enumeration for defining user role categories.
+ *
+ * @author Preznyák László
+ * @version 1.0
+ */
+
 public enum Roles {
     USER, ADMIN
 }
--- src/main/java/hu/preznyak/enums/ServiceType.java
+++ src/main/java/hu/preznyak/enums/ServiceType.java
@@ -1,5 +1,13 @@
 package hu.preznyak.enums;
 
+/**
+ * <h1>ServiceType Enumeration for service types.</h1>
+ * This is an enum for service types.
+ *
+ * @author Preznyák László
+ * @version 1.0
+ */
+
 public enum  ServiceType {
     BAND, PHOTOGRAPHER, VIDEO, MASTER_OF_CEREMONIES, VENUE, RESTAURANT
 }
