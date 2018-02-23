ba2749d081803428e2786fc13d2e2d154c07fdba
Preznyák László <laszlopreznyak11@gmail.com>
2018-02-23 10:56:49 +0200

    Javadoc added to NavigationMB.java

--- src/main/java/hu/preznyak/managedbeans/NavigationMB.java
+++ src/main/java/hu/preznyak/managedbeans/NavigationMB.java
@@ -2,27 +2,61 @@ package hu.preznyak.managedbeans;
 
 import javax.faces.bean.ManagedBean;
 
+/**
+ * <h1>NavigationMB ManagedBean class.</h1>
+ * A class for navigation.
+ *
+ * @author Preznyák László
+ * @version 1.0
+ */
+
 @ManagedBean(name = "NavigationMB")
 public class NavigationMB {
+
+    /**
+     * Method for sending the application to the home page.
+     * @return String address of the home page.
+     */
     public String goHome(){
         return "/home";
     }
 
+    /**
+     * Method for sending the application to the index page.
+     * @return String address of the index page.
+     */
     public String goToIndex(){
         return "/index";
     }
 
+    /**
+     * Method for sending the application to the myServices page.
+     * @return String address of the myServices page.
+     */
     public String goToMyServices(){
         return "/myServices";
     }
 
+    /**
+     * Method for sending the application to the services page.
+     * @return String address of the services page.
+     */
     public String goToAllServices(){
         return "/services";
     }
+
+    /**
+     * Method for sending the application to the createService page.
+     * @return String address of the createService page.
+     */
     public String goToCreateService(){
         return "/createService";
     }
 
+    /**
+     * Method for sending the application to the login page.
+     * @return String address of the login page.
+     */
     public String goToLogin(){
         return "/login";
     }
