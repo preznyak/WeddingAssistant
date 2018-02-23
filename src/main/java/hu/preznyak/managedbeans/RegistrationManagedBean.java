06bad0c6845de41f06c503f74d985d53fe9c16a7
Preznyák László <laszlopreznyak11@gmail.com>
2018-02-23 10:59:50 +0200

    Javadoc added to RegistrationManagedBean.java

--- src/main/java/hu/preznyak/managedbeans/RegistrationManagedBean.java
+++ src/main/java/hu/preznyak/managedbeans/RegistrationManagedBean.java
@@ -8,11 +8,26 @@ import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
 import javax.faces.context.FacesContext;
 
+/**
+ * <h1>RegistrationManagedBean class.</h1>
+ * This is a managed bean class for registration.
+ *
+ * @author Preznyák László
+ * @version 1.0
+ */
+
 @ManagedBean(name = "RegistrationMB")
 @RequestScoped
 public class RegistrationManagedBean {
+    /**
+     * A {@link User} object. In this object will be stored the data from the registration.
+     */
     User user = new User();
 
+    /**
+     * register method for registration.
+     * @return String a navigation String.
+     */
     public String register(){
         UserDAOImpl userDAO = new UserDAOImpl();
         if(userDAO.addUser(user)) {
@@ -25,10 +40,18 @@ public class RegistrationManagedBean {
         }
     }
 
+    /**
+     * Getter method for the user.
+     * @return user a {@link User} object.
+     */
     public User getUser() {
         return user;
     }
 
+    /**
+     * Setter method for the user.
+     * @param user a {@link User} object.
+     */
     public void setUser(User user) {
         this.user = user;
     }
