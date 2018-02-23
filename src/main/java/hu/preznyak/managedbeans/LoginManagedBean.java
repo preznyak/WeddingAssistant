5c6793074971f4f0a3476898a206e20971c52598
Preznyák László <laszlopreznyak11@gmail.com>
2018-02-23 10:49:29 +0200

    Javadoc added to LoginManagedBean.java

--- src/main/java/hu/preznyak/managedbeans/LoginManagedBean.java
+++ src/main/java/hu/preznyak/managedbeans/LoginManagedBean.java
@@ -11,14 +11,32 @@ import javax.faces.context.FacesContext;
 import javax.servlet.http.HttpSession;
 import java.io.Serializable;
 
+/**
+ * <h1>LoginManagedBean ManagedBean class.</h1>
+ * This is a managed bean class for login method.
+ *
+ * @author Preznyák László
+ * @version 1.0
+ */
+
 @ManagedBean(name="LoginMB")
 @SessionScoped
 public class LoginManagedBean implements Serializable{
 
+    /**
+     * A boolean value which stores that a user is logged in or not.
+     */
     private boolean loggedIn;
 
+    /**
+     * A {@link User} object.
+     */
     private User user = new User();
 
+    /**
+     * Login method for logging in the application.
+     * @return String a String for navigation.
+     */
     public String login(){
         UserDAOImpl userDAO = new UserDAOImpl();
         user = userDAO.getUser(user.getUsername(), user.getPassword());
@@ -37,6 +55,10 @@ public class LoginManagedBean implements Serializable{
         }
     }
 
+    /**
+     * Logout method for logging out of the application.
+     * @return String a String for navigation.
+     */
     public String logout(){
         HttpSession session = SessionUtils.getSession();
         session.invalidate();
@@ -44,22 +66,41 @@ public class LoginManagedBean implements Serializable{
         return "/index";
     }
 
+    /**
+     * Getter method for the user.
+     * @return user a {@link User} object.
+     */
     public User getUser() {
         return user;
     }
 
+    /**
+     * Setter method for the user.
+     * @param user a {@link User} object.
+     */
     public void setUser(User user) {
         this.user = user;
     }
 
+    /**
+     * Getter method for the loggedIn variable.
+     * @return loggedIn a boolean value.
+     */
     public boolean isLoggedIn() {
         return loggedIn;
     }
 
+    /**
+     * Setter method for the loggedIn variable.
+     * @param loggedIn a boolean value.
+     */
     public void setLoggedIn(boolean loggedIn) {
         this.loggedIn = loggedIn;
     }
 
+    /**
+     * No-arg constructor for the {@link LoginManagedBean} class.
+     */
     public LoginManagedBean() {
     }
 }
