a86e6adbb682113674331c5940d40c5d5c7dcdc6
Preznyák László <laszlopreznyak11@gmail.com>
2018-02-23 11:11:09 +0200

    Javadoc added to UpdateServiceMB.java

--- src/main/java/hu/preznyak/managedbeans/UpdateServiceMB.java
+++ src/main/java/hu/preznyak/managedbeans/UpdateServiceMB.java
@@ -7,30 +7,60 @@ import hu.preznyak.entities.Service;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 
+/**
+ * <h1>UpdateServiceMB ManagedBean class.</h1>
+ * This is a managed bean class for updating services.
+ *
+ * @author Preznyák László
+ * @version 1.0
+ */
+
 @ManagedBean(name = "UpdateServiceMB")
 @SessionScoped
 public class UpdateServiceMB {
 
-    private Service updateableService;
+    /**
+     * A {@link Service} object can be updated.
+     */
+    private Service updatableService;
 
+    /**
+     * selectServiceForUpdate method for selecting a {@link Service} for update.
+     * @param service a {@link Service} object.
+     * @return String a navigation String
+     */
     public String selectServiceForUpdate(Service service){
-        updateableService = service;
+        updatableService = service;
         return "/updateMyService";
     }
 
+    /**
+     * updateService method for updating the updatableService object.
+     */
     public void updateService(){
         ServiceDAO serviceDAO = new ServiceDAOImpl();
-        serviceDAO.updateService(updateableService);
+        serviceDAO.updateService(updatableService);
     }
 
+    /**
+     * No-arg constructor for {@link UpdateServiceMB} class.
+     */
     public UpdateServiceMB() {
     }
 
-    public Service getUpdateableService() {
-        return updateableService;
+    /**
+     * Getter method for updatableService.
+     * @return updatableService a {@link Service} object.
+     */
+    public Service getUpdatableService() {
+        return updatableService;
     }
 
-    public void setUpdateableService(Service updateableService) {
-        this.updateableService = updateableService;
+    /**
+     * Setter method for updatableService.
+     * @param updatableService a {@link Service} object.
+     */
+    public void setUpdatableService(Service updatableService) {
+        this.updatableService = updatableService;
     }
 }
