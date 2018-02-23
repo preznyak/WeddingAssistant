375c3d193203654be100576b71c2218d3f12f6fa
Preznyák László <laszlopreznyak11@gmail.com>
2018-02-23 10:53:11 +0200

    Javadoc added to MyServicesMB.java

--- src/main/java/hu/preznyak/managedbeans/MyServicesMB.java
+++ src/main/java/hu/preznyak/managedbeans/MyServicesMB.java
@@ -12,12 +12,26 @@ import javax.faces.bean.RequestScoped;
 import javax.servlet.http.HttpSession;
 import java.util.List;
 
+/**
+ * <h1>MyServicesMB ManagedBean class.</h1>
+ * This is a class for listing and manipulating a user's services.
+ *
+ * @author Preznyák László
+ * @version 1.0
+ */
+
 @ManagedBean(name = "MyServices")
 @RequestScoped
 public class MyServicesMB {
 
+    /**
+     * A list of {@link Service} objects.
+     */
     private List<Service> myServicesList;
 
+    /**
+     * Init method of the {@link MyServicesMB} class.
+     */
     @PostConstruct
     public void init(){
         HttpSession session = SessionUtils.getSession();
@@ -26,6 +40,10 @@ public class MyServicesMB {
         myServicesList = user.getServiceList();
     }
 
+    /**
+     * removeMyService method for removing a {@link Service} from the myServicesList list.
+     * @param id the id of a {@link Service} object.
+     */
     public void removeMyService(int id){
         HttpSession session = SessionUtils.getSession();
         UserDAO userDAO = new UserDAOImpl();
@@ -34,14 +52,24 @@ public class MyServicesMB {
         init();
     }
 
-
+    /**
+     * No-arg constructor for {@link MyServicesMB} class.
+     */
     public MyServicesMB() {
     }
 
+    /**
+     * Getter method for the myServicesList.
+     * @return myServicesList the list of services.
+     */
     public List<Service> getMyServicesList() {
         return myServicesList;
     }
 
+    /**
+     * Setter method for the myServicesList.
+     * @param myServicesList a list of {@link Service} objects.
+     */
     public void setMyServicesList(List<Service> myServicesList) {
         this.myServicesList = myServicesList;
     }
