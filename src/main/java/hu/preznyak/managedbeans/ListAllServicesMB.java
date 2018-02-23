bb96c86eee74330a8cef8e2e6d813ae9bd7eaa0a
Preznyák László <laszlopreznyak11@gmail.com>
2018-02-23 10:43:59 +0200

    Javadoc addded to ListAllServicesMB.java

--- src/main/java/hu/preznyak/managedbeans/ListAllServicesMB.java
+++ src/main/java/hu/preznyak/managedbeans/ListAllServicesMB.java
@@ -1,29 +1,53 @@
 package hu.preznyak.managedbeans;
 
 import hu.preznyak.daos.ServiceDAO;
-import hu.preznyak.daos.UserDAO;
 import hu.preznyak.daos.impls.ServiceDAOImpl;
-import hu.preznyak.daos.impls.UserDAOImpl;
 import hu.preznyak.entities.Service;
 
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
 import java.util.List;
 
+/**
+ * <h1>ListALlServicesMB ManagedBean class.</h1>
+ * This is a managed bean for listing all the registered services from the database.
+ *
+ * @author Preznyák László
+ * @version 1.0
+ */
+
 @ManagedBean(name = "ServicesListMB")
 @ViewScoped
 public class ListAllServicesMB {
 
+    /**
+     * The {@link ServiceDAO} that we can use for db operations.
+     */
     private ServiceDAO serviceDAO = new ServiceDAOImpl();
+
+    /**
+     * The {@link Service} list.
+     */
     private List<Service> serviceList = serviceDAO.getAllServices();
 
+    /**
+     * No-arg constructor for {@link ListAllServicesMB} class.
+     */
     public ListAllServicesMB() {
     }
 
+    /**
+     * Getter method for the serviceList.
+     * @return serviceList the list of services.
+     */
     public List<Service> getServiceList() {
         return serviceList;
     }
 
+    /**
+     * Setter method for the serviceList.
+     * @param serviceList the service list.
+     */
     public void setServiceList(List<Service> serviceList) {
         this.serviceList = serviceList;
     }
