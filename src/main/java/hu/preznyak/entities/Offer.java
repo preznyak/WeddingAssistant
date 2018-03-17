1a98e676d3f890789b48e6278d3af93831356057
Preznyák László <laszlopreznyak11@gmail.com>
2018-03-17 16:03:26 +0200

    Offer.java refactored, javadoc added.

--- src/main/java/hu/preznyak/entities/Offer.java
+++ src/main/java/hu/preznyak/entities/Offer.java
@@ -2,19 +2,56 @@ package hu.preznyak.entities;
 
 import javax.persistence.*;
 
+/**
+ * <h1>Offer class</h1>
+ *
+ * @author Preznyák László
+ * @version 1.0
+ */
+
 @Table
 @Entity(name ="offer")
 public class Offer {
 
+    /**
+     * The id of the Offer.
+     */
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private int id;
 
-    @OneToOne(targetEntity = User.class)
-    private User user;
+    /**
+     * The {@link WeddingEvent} which needs the {@link Service}.
+     */
+    @OneToOne(targetEntity = WeddingEvent.class, fetch = FetchType.EAGER)
+    private WeddingEvent weddingEvent;
 
+    /**
+     * The {@link Service} which is ordered by a {@link WeddingEvent}.
+     */
+    @OneToOne(targetEntity = Service.class, fetch = FetchType.EAGER)
+    private  Service service;
+
+    /**
+     * Note.
+     */
     @Column(nullable = false)
-    private String description;
+    private String note;
+
+    /**
+     * The price of the {@link Service}.
+     */
+    @Column
+    private double price;
+
+    /**
+     * A boolean value which shows, if the Offer was accepted by the {@link WeddingEvent} owner, or not.
+     */
+    @Column
+    private boolean accepted;
+
+    public Offer() {
+    }
 
     public int getId() {
         return id;
@@ -24,22 +61,55 @@ public class Offer {
         this.id = id;
     }
 
-    public User getUser() {
-        return user;
+    public WeddingEvent getWeddingEvent() {
+        return weddingEvent;
     }
 
-    public void setUser(User user) {
-        this.user = user;
+    public void setWeddingEvent(WeddingEvent weddingEvent) {
+        this.weddingEvent = weddingEvent;
     }
 
-    public String getDescription() {
-        return description;
+    public Service getService() {
+        return service;
     }
 
-    public void setDescription(String description) {
-        this.description = description;
+    public void setService(Service service) {
+        this.service = service;
     }
 
-    public Offer() {
+    public String getNote() {
+        return note;
+    }
+
+    public void setNote(String note) {
+        this.note = note;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public boolean isAccepted() {
+        return accepted;
+    }
+
+    public void setAccepted(boolean accepted) {
+        this.accepted = accepted;
+    }
+
+    @Override
+    public String toString() {
+        return "Offer{" +
+                "id=" + id +
+                ", weddingEvent=" + weddingEvent +
+                ", service=" + service +
+                ", note='" + note + '\'' +
+                ", price=" + price +
+                ", accepted=" + accepted +
+                '}';
     }
 }
